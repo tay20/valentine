ca5c4c644c50c3301f981bb80e31a8361b9ff8c6
tay20 <70497508+tay20@users.noreply.github.com>
2026-02-04 19:20:25 -0600

    Update script.js
    changed text

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
       yesButton.style.fontSize = `${fontSize}px`;
   
       //no button text
-      const messages = ["No","Are you sure?","Babyy please?","Don't do this to me :(","Say yes or else...",];
+      const messages = ["No","Are you sure?","Awwww please?","Don't do this to me :(","Say yes or else...",];
   
       if (noClickCount === 4) {
         const newButton = document.createElement("button");
@@ -156,3 +156,4 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
   }
 
   
+
